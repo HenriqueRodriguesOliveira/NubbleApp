04a296dec2704f9c6785f205bb6370d8209dd2b0
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2025-04-19 11:03:57 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native';
+import React from 'react';
+import { Text } from './src/components/Text/Text';
+
+export default function App() {
+  return (
+    <View>
+      <Text preset="headingLarge" style={{ color: 'red' }}>Aoba</Text>
+    </View>
+  );
+}

2a066a7db08188bf63af6a3381d3533be9ca1fac
Henrique Rodrigues Oliveira <henrique_krf@outlook.com>
2025-04-23 04:16:37 -0300

    Component Button Restyle

--- App.tsx
+++ App.tsx
@@ -1,11 +1,21 @@
 import { View } from 'react-native';
 import React from 'react';
 import { Text } from './src/components/Text/Text';
+import { Button } from './src/components/Button/Button';
+import { Box } from './src/components/Box/Box';
+import { ThemeProvider } from '@shopify/restyle';
+import { theme } from './src/theme/theme';
 
 export default function App() {
   return (
-    <View>
-      <Text preset="headingLarge" style={{ color: 'red' }}>Aoba</Text>
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={{ paddingHorizontal: 24 }}>
+        <Text preset="headingLarge" italic>CoffStack</Text>
+        <Box marginBottom="s24">
+          <Button title="Entrar" />
+        </Box>
+        <Button loading title="Loading" />
+      </View>
+    </ThemeProvider>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { Text } from '../Text/Text';
+import { TouchableOpacityBox, TouchableOpacityBoxProps } from '../Box/Box';
+
+interface ButtonProps extends TouchableOpacityBoxProps {
+    title: string;
+    loading?: boolean;
+}
+
+export function Button({ title, loading, ...TouchableOpacityBoxProps }: ButtonProps) {
+    return (
+        <TouchableOpacityBox
+            backgroundColor="buttonPrimary"
+            paddingHorizontal="s20"
+            height={50}
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="s16"
+            {...TouchableOpacityBoxProps}
+        >
+            {loading ? <ActivityIndicator /> :
+                <Text preset="paragraphMedium" style={{ color: '#FFF' }}>{title}</Text>
+            }
+        </TouchableOpacityBox>
+    );
+}
